afa2ecb0dcc451c1f95e165f8f92ac72b9d04bb7
Hadla Bergman <hadla.bergman@einride.tech>
2023-03-06 20:53:34 +0100

    feat: add page title and post content

--- splayoneTheme/front-page.php
+++ splayoneTheme/front-page.php
@@ -1,11 +1,19 @@
 <?php get_header(); ?>
 
-    <section class="page">
+    <section class="row">
         <div class="container">
             <h1>Front-page: <?php the_title(); ?></h1>
+            <?php get_template_part('includes/section', 'intro'); ?>
+            <?php 
+                if( have_posts() ){
+                    while( have_posts() ){
+                        the_post();
+                        the_content();
+                    }
+                }
+            ?>
         </div>
 
-        <?php get_template_part('includes/section', 'intro'); ?>
     </section>
 
 <?php get_footer(); ?>
